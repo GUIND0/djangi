33c46955e90e85faf56c66460cc4aaadb0bc6634
Souleymane <s.guindo@mdcmali.com>
2022-07-26 21:30:01 +0000

    registration done

--- src/main/java/ml/djangi/app/Djangi/service/UserService.java
+++ src/main/java/ml/djangi/app/Djangi/service/UserService.java
@@ -2,7 +2,8 @@ package ml.djangi.app.Djangi.service;
 
 import ml.djangi.app.Djangi.model.User;
 import ml.djangi.app.Djangi.web.dto.UserRegistrationDto;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
-public interface UserService {
+public interface UserService extends UserDetailsService {
     User save(UserRegistrationDto userRegistrationDto);
 }
